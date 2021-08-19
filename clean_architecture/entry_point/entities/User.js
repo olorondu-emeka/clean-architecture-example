1a82aa9c749c0b95ab680114f84415607a3bb0a5
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2021-08-19 16:21:19 +0100

    created folder structure for clean architecture

---
+++ clean_architecture/entry_point/entities/User.js
@@ -0,0 +1,10 @@
+class User {
+  constructor(lastName, firstName, email, password) {
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+module.exports = User;
