13830d2247aa496eeacd3654b4415ddacfe34ecc
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2021-08-20 16:18:54 +0100

    updated error and success response

--- clean_architecture/entry_point/definitions/ErrorResponse.js
+++ clean_architecture/entry_point/definitions/ErrorResponse.js
@@ -1,3 +1,14 @@
+function getResponseObject(message, data) {
+  return {
+    statusCode: 500,
+    responseObject: {
+      status: 'success',
+      message,
+      data
+    }
+  };
+}
+
 class ErrorResponse {}
 
 module.exports = ErrorResponse;
--- clean_architecture/entry_point/definitions/SuccessResponse.js
+++ clean_architecture/entry_point/definitions/SuccessResponse.js
@@ -1,3 +1,22 @@
-class SuccessResponse {}
+function getResponseObject(statusCode, message, data) {
+  return {
+    statusCode,
+    responseObject: {
+      status: 'success',
+      message,
+      data
+    }
+  };
+}
+
+class SuccessResponse {
+  static ok(message, data) {
+    return getResponseObject(200, message, data);
+  }
+
+  static created(message, data) {
+    return getResponseObject(201, message, data);
+  }
+}
 
 module.exports = SuccessResponse;
