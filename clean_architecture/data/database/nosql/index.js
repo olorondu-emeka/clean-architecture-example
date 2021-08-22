3869b582fc183a157eb2f7706e61826867665b49
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2021-08-22 19:55:23 +0100

    chore: added env.sample file

--- clean_architecture/data/database/nosql/index.js
+++ clean_architecture/data/database/nosql/index.js
@@ -6,9 +6,9 @@ class MongoDB {
 
     mongoose.set('useCreateIndex', true);
     mongoose.set('useNewUrlParser', true);
+    mongoose.set('useUnifiedTopology', true);
 
     // mongoose.set('useFindAndModify', false);
-    // mongoose.set('useUnifiedTopology', true);
 
     return mongoose.connect(this.connectionString);
   }
